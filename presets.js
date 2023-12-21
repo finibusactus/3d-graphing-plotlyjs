2ade6d5b32ebd1fc4770a27ca856a5975bdfc330
finibusactus <154544638+finibusactus@users.noreply.github.com>
2023-12-21 17:14:38 +0000

    Add files via upload

---
+++ presets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const sineGraph = {formula:"z = sin(x) + sin(y)", startNumber: "0", endNumber: "10", step: "0.1"};
+const leafGraph = {formula:"z = x^3+x^2+x+1 + y^3+y^2+y+1", startNumber: "-20", endNumber: "20", step: "1"};
+const fuzzySineGraph = {formula:"z = random() + sin(y-x) + cos(x+y)", startNumber: "0", endNumber: "10", step: "0.07"};
+const HillGraph = {formula:"z = random(x*y)", startNumber: "0", endNumber: "100", step: "1"};
+const fuzzyHillGraph = {formula:"z = random() + sin(y-x) + cos(x+y) + random(sqrt(y))", startNumber: "0", endNumber: "10", step: "0.07"};
+const rippleGraph = {formula:"z=((-e^(-x*y/1)*cos( (x^2+y^2)/10 ))+14*log(10000/(x^2+y^2)+.01))*( cos(x^2+y^2)/10)+3*((x)-(x))*((y)-(y))", startNumber: "-3", endNumber: "3", step: "0.07"};
+
+const dataPresets = {"Sine Graph":sineGraph, "Leaf Graph":leafGraph, "Fuzzy Sine Graph":fuzzySineGraph, 'Hill Graph':HillGraph, "Fuzzy Hill Graph":fuzzyHillGraph, 'Ripple Graph': rippleGraph};
+
+//https://www.physicsforums.com/threads/cool-3-d-functions-for-graphing.140087/ [rippleGraph]
+
+
+export {dataPresets};
---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import { dataPresets } from "./presets.js";
+
+function plotGraph(){
+  let formula = document.getElementById("formula").value;
+  let startNumber = Number(document.getElementById("startNumber").value);
+  let endNumber = Number(document.getElementById("endNumber").value);
+  let step = Number(document.getElementById("step").value);
+  let xData = [];
+  let yData = [];
+  let zData = [];
+  for (let n = startNumber; n < endNumber; n += step){
+    xData.push(n);
+    yData.push(n);
+  }
+  for (let y = startNumber; y < endNumber; y += step){
+    let localXZData = [];
+    for (let x = startNumber; x < endNumber; x += step){
+      let scope = {x:x, y:y};
+      math.evaluate(formula, scope);
+      localXZData.push(scope.z);
+    }
+    zData.push(localXZData);
+  }
+  Plotly.react(graphDiv, [{type:"surface", x:xData, y:yData, z:zData}], {uirevision:'true'})
+}
+
+function loadDropDownPresets(){
+  let option = document.getElementById('selectPresets').value;
+  let data = dataPresets[option];
+  document.getElementById("formula").value = data.formula;
+  document.getElementById("startNumber").value = data.startNumber;
+  document.getElementById("endNumber").value = data.endNumber;
+  document.getElementById("step").value = data.step;
+}
+
+
+
+
+document.getElementById("plotButton").addEventListener("click", function(event){plotGraph();event.preventDefault;});
+document.getElementById("selectPresets").addEventListener("click", function(event){loadDropDownPresets();event.preventDefault;});
+Plotly.newPlot(graphDiv, [{type: "surface", z:[[0,1,2,3,2,1,1],[1,2,4,2,1,1,1],[1,2,5,2,1,1,1]]}])
